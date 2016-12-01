5354d9b4188e11bab6b551b39187d0860d6cab7e
Bruce <bruce@redravencomputing.com>
2016-11-30 20:57:18 -0700

    add basic drawing functionality

--- Easel/ViewController.swift
+++ Easel/ViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var buttonsStackView: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var lastPoint = CGPoint.zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,19 +31,44 @@ class ViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Began")
+        if let touch = touches.first {
+            let point = touch.location(in: self.imageView)
+            self.lastPoint = point
+        }
+        
+        
         self.buttonsStackView.isHidden = true
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Moved")
+        if let touch = touches.first {
+            let point = touch.location(in: self.imageView)
+            print(point)
+            
+            
+            UIGraphicsBeginImageContext(self.imageView.frame.size)
+            let context = UIGraphicsGetCurrentContext()
+            
+            self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+            
+            context?.move(to: self.lastPoint)
+            context?.addLine(to: point)
+            
+            context?.strokePath()
+            
+            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            
+            UIGraphicsEndImageContext()
+            
+            self.lastPoint = point
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("Ended")
         self.buttonsStackView.isHidden = false
     }
-
-
+    
+    
 }
 
