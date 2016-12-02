d8a3544a99d5819753d107700da558f721f83a54
Bruce <bruce@redravencomputing.com>
2016-12-01 23:00:05 -0700

    Finish class project

--- Easel/DrawingViewController.swift
+++ Easel/DrawingViewController.swift
@@ -14,12 +14,17 @@ class DrawingViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var lastPoint = CGPoint.zero
+    var red : CGFloat = 0.0
+    var green : CGFloat = 0.0
+    var blue : CGFloat = 0.0
+    var brushSize : CGFloat = 1.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         NotificationCenter.default.addObserver(self, selector: #selector(DrawingViewController.appBecameActive), name: Notification.Name.UIApplicationDidBecomeActive, object: nil)
+        blueTapped(UIButton())
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,32 +50,42 @@ class DrawingViewController: UIViewController {
             let point = touch.location(in: self.imageView)
             print(point)
             
-            
-            UIGraphicsBeginImageContext(self.imageView.frame.size)
-            let context = UIGraphicsGetCurrentContext()
-            
-            self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
-            
-            context?.move(to: self.lastPoint)
-            context?.addLine(to: point)
-            
-            context?.setStrokeColor(red: 0.3, green: 0.8, blue: 0.2, alpha: 1.0)
-            context?.setLineCap(.round)
-            context?.setLineWidth(15)
-            
-            context?.strokePath()
-            
-            self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
-            
-            UIGraphicsEndImageContext()
+            drawBetweenPoints(firstPoint: self.lastPoint, secondPoint: point)
             
             self.lastPoint = point
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("Ended")
+        if let touch = touches.first {
+            let point = touch.location(in: self.imageView)
+            print(point)
+            
+            drawBetweenPoints(firstPoint: self.lastPoint, secondPoint: point)
+        }
+
         self.buttonsStackView.isHidden = false
+    }
+    
+    func drawBetweenPoints(firstPoint: CGPoint, secondPoint: CGPoint) {
+        UIGraphicsBeginImageContext(self.imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        self.imageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.imageView.frame.size.width, height: self.imageView.frame.size.height))
+        
+        context?.move(to: firstPoint)
+        context?.addLine(to: secondPoint)
+        
+        context?.setStrokeColor(red: self.red, green: self.green, blue: self.blue, alpha: 1.0)
+        context?.setLineCap(.round)
+        context?.setLineWidth(brushSize)
+        
+        context?.strokePath()
+        
+        self.imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+
     }
     
     func earseEasel() {
@@ -83,6 +98,38 @@ class DrawingViewController: UIViewController {
             settingsVC.drawingVC = self
         }
     }
+    
+    
+    @IBAction func blueTapped(_ sender: Any) {
+        self.red = 56/255
+        self.green = 109/255
+        self.blue = 229/255
+    }
+    
+    @IBAction func greenTapped(_ sender: Any) {
+        self.red = 37/255
+        self.green = 235/255
+        self.blue = 114/255
+    }
+    
+    @IBAction func redTapped(_ sender: Any) {
+        self.red = 229/255
+        self.green = 56/255
+        self.blue = 56/255
+    }
+    
+    @IBAction func yellowTapped(_ sender: Any) {
+        self.red = 249/255
+        self.green = 215/255
+        self.blue = 23/255
+    }
+    
+    @IBAction func randomTapped(_ sender: Any) {
+        self.red = CGFloat(arc4random_uniform(256))/255
+        self.green = CGFloat(arc4random_uniform(256))/255
+        self.blue = CGFloat(arc4random_uniform(256))/255
+    }
+    
     
     
 }
--- Easel/SettingsViewController.swift
+++ Easel/SettingsViewController.swift
@@ -12,10 +12,19 @@ class SettingsViewController: UIViewController {
     
     weak var drawingVC : DrawingViewController? = nil
 
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let brushSize = self.drawingVC?.brushSize {
+            self.slider.value = Float(brushSize)
+        }
+        
     }
 
     @IBAction func eraseTapped(_ sender: Any) {
@@ -23,4 +32,15 @@ class SettingsViewController: UIViewController {
         _ = self.navigationController?.popViewController(animated: true)
     }
 
+    @IBAction func sharedTapped(_ sender: Any) {
+        if let image = self.drawingVC?.imageView.image {
+            let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            self.present(activityVC, animated: true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        self.drawingVC?.brushSize = CGFloat(self.slider.value)
+    }
 }
